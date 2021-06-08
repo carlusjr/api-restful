17b8398230708f2869ffb940f5c3887fb2973803
carlusjr <crsilvajr@gmail.com>
2021-06-08 16:34:51 -0300

    add CORS

--- src/server.js
+++ src/server.js
@@ -1,5 +1,7 @@
 const express = require("express");
-//const path = require("path");
+
+// Dependência para configuração de domínios que podem acessar as APIs deste servidor
+const cors = require("cors");
 
 const db = require("./database/db");
 const routes = require("./routes/routes");
@@ -12,12 +14,26 @@ app.use(express.json());
 // Conectando bando de dados MongoDB
 db.connectMongoDB();
 
+// habilita CORS
+// Para todos os domínios = app.use(cors());
+// Para apenas um domínio = app.use(cors({ origin: "http://127.0.0.1:5500" }));
+// Para mais de um endereço
+
+const allowedOrigins = ["http://127.0.0.1:5500", "https://www.frontend.com.br"];
+app.use(
+  cors({
+    origin: function(origin, callback) {      
+      const allowed = !origin ? true : allowedOrigins.includes(origin);      
+      callback(null, allowed); // primeiro parâmetro é uma mensagem, quando null retorna mensagem padrão
+    },
+  })
+);
 
 // definindo rotas atendidas pelo servidor
 app.use("/api", routes);
 
-
 // executando o servidor
 const port = process.env.PORT || 8080;
-app.listen(port, () => console.log(`API restful server running on port ${port}.`));
-
+app.listen(port, () =>
+  console.log(`API restful server running on port ${port}.`)
+);
